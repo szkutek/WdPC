ebbbd28ec1f9f77360977a05dfebb9dd40aca14a
szkutek <aga.szkutek@gmail.com>
2016-11-04 09:05:03 +0100

    zad2 finished
    when <0, just print -

--- prac3/zad2/main.c
+++ prac3/zad2/main.c
@@ -1,16 +1,21 @@
+//290207
+//LISTA 3
+//ZADANIE 2
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
 
-#define MAX_TAB 33
+#define MAX_TAB 34
 
 int is_letter(char c){
     return (c >= 'A' && c <= 'Z');
 }
 
 int convert_to_base10(int nlen, int base, char n[nlen]){
-    int num10=0, pot=1, i=0;
+    unsigned int num10=0;
+    int pot=1, i=0;
     for (i=nlen; i>0; i--){
         if (is_letter(n[i-1])) num10 += (n[i-1]-'A'+10)*pot;
         else                   num10 += (n[i-1]-'0')*pot;
@@ -19,7 +24,7 @@ int convert_to_base10(int nlen, int base, char n[nlen]){
     return num10;
 }
 
-int convert_to_new_base(int num10, int new_base, char res[]){
+int convert_to_new_base(unsigned int num10, int new_base, char res[]){
     int j=0, tmp=0;
     while (num10>0){
         tmp = num10%new_base;
@@ -38,7 +43,8 @@ int main(void)
     int base=0, new_base=0;
     char n[MAX_TAB]={0};
 
-    int num10=0;
+    int sgn=0;
+    unsigned int num10=0;
 
     char res[MAX_TAB]={0};
     int m=0;
@@ -47,24 +53,30 @@ int main(void)
     printf("Podaj stary i nowy system pozycyjny oraz liczbe naturalna:\n");
     scanf("%d %d %s", &base, &new_base, n);
 
-    //length(n) = nlen+1
-    int i=0;
-    while (n[i]!='\0') i++;
 
-    printf("%d\n", i);
+    int i=0;
+    while (n[i]!='\0') i++; //length(n)-1
 
     if (i==1 && n[0]=='0') puts("0");
-    else {
-        num10 = convert_to_base10(i, base, n);
-
-        m = convert_to_new_base(num10, new_base, res);
-
-        // print results
-        for (int k=0; k<m+1; k++){
-            printf("%c", res[m-k]);
+    else
+        if (n[0]=='-'){
+            sgn=1;
+            n[0]='0';
         }
-        puts("");
-    }
+        if ((base>1 && base<17) && (new_base>1 && new_base<17)){
+            num10 = convert_to_base10(i, base, n);
+
+            if (num10>0){
+                m = convert_to_new_base(num10, new_base, res);
+
+                // print results
+                if (sgn==1) printf("-");
+                for (int k=0; k<m+1; k++){
+                    printf("%c", res[m-k]);
+                }
+                puts("");
+            }
+        }
 
     return 0;
 }
