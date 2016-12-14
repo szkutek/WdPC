65890ac50e5ed51940959977a21ca97b67ae6859
szkutek <aga.szkutek@gmail.com>
2016-12-14 22:26:22 +0100

    prawie dobrze

--- prac7/zad3/main.c
+++ prac7/zad3/main.c
@@ -3,22 +3,37 @@
 
 #define MAXN 100
 
-int N=1, M=1; //mapa
-int P=0, L=0; //sekwencja
 
-char mapa[MAXN][MAXN+1]={{0}};
+typedef struct{
+    char wart;
+    int G;
+    int D;
+    int L;
+    int P;
+}PUNKT;
+
+int N=1, M=1; //mapa
+int P=0; // ilosc krokow
+int L=0; // dlugosc sekwencji
+
+PUNKT mapa[MAXN][MAXN+1];
 char sekwencja[MAXN+1]={0};
 
 int directions[4] = { 0, 0, 0, 0};
                 //   G  D  L  P
 
+
 void read_data(void){
 
-    scanf("%d", &N);
-    scanf("%d", &M);
+    scanf("%d ", &N);
+    scanf("%d ", &M);
 
+//    for (int i=0; i<M; i++){    scanf("%s", mapa[i]);    }
     for (int i=0; i<M; i++){
-         scanf("%s", mapa[i]);
+        for (int j=0; j<N+1; j++){
+            mapa[i][j].wart = getchar();
+        }
+        mapa[i][N].wart = '\0';
     }
 
     scanf("%d", &P);
@@ -34,69 +49,100 @@ void read_data(void){
 
 void print_map(void){
     for (int i=0; i<M; i++){
-        printf("%s\n", mapa[i]);
+        for (int j=0; j<N; j++){
+             printf("%c", mapa[i][j].wart);
+        }
+        printf("\n");
     }
 }
 
 int eval_move(int i, int j, int start_path, int moves_num){
 
-    if (start_path==L && moves_num==P && (mapa[i][j]=='.' || mapa[i][j]=='X')){
-        mapa[i][j] = 'X';
+    if (start_path==L && moves_num==P){
+        mapa[i][j].wart = 'X';
         return 1;
     }
 
     switch (sekwencja[start_path]){ // dodaj warunki zeby nie wyjsc poza mape!
         case 'G':
-            if ((mapa[i-1][j  ]=='.' || mapa[i-1][j  ] =='X') && !directions[0] )
-                eval_move(i-1, j  , start_path+1, moves_num+1);
+            if ((mapa[i-1][j  ].wart=='.' || mapa[i-1][j  ].wart =='X') && !mapa[i-1][j  ].G ){
+                 mapa[i-1][j  ].G = 1;
+                if (!eval_move(i-1, j  , start_path+1, moves_num+1)) mapa[i-1][j  ].G = 0;
+            }
             break;
         case 'D':
-            if ((mapa[i+1][j  ]=='.' || mapa[i+1][j  ] =='X') && !directions[1] )
-                eval_move(i+1, j  , start_path+1, moves_num+1);
+            if ((mapa[i+1][j  ].wart=='.' || mapa[i+1][j  ].wart =='X') && !mapa[i+1][j  ].D ){
+                 mapa[i+1][j  ].D = 1;
+                if (!eval_move(i+1, j  , start_path+1, moves_num+1)) mapa[i+1][j  ].D = 0;
+            }
             break;
         case 'L':
-            if ((mapa[i  ][j-1]=='.' || mapa[i  ][j-1] =='X') && !directions[2] )
-                eval_move(i  , j-1, start_path+1, moves_num+1);
+            if ((mapa[i  ][j-1].wart=='.' || mapa[i  ][j-1].wart =='X') && !mapa[i  ][j-1].L ){
+                 mapa[i  ][j-1].L = 1;
+                if (!eval_move(i  , j-1, start_path+1, moves_num+1)) mapa[i  ][j-1].L = 0;
+            }
             break;
         case 'P':
-            if ((mapa[i  ][j+1]=='.' || mapa[i  ][j+1] =='X') && !directions[3] )
-                eval_move(i  , j+1, start_path+1, moves_num+1);
+            if ((mapa[i  ][j+1].wart=='.' || mapa[i  ][j+1].wart =='X') && !mapa[i  ][j+1].P ){
+                 mapa[i  ][j+1].P = 1;
+                if (!eval_move(i  , j+1, start_path+1, moves_num+1)) mapa[i  ][j+1].P = 0;
+            }
             break;
         case 'S':
                 eval_move(i  , j  , start_path+1, moves_num);
             break;
         case '?':
-            eval_move(i-1, j  , start_path+1, moves_num+1);
-            eval_move(i+1, j  , start_path+1, moves_num+1);
-            eval_move(i  , j-1, start_path+1, moves_num+1);
-            eval_move(i  , j+1, start_path+1, moves_num+1);
+
+            if ((mapa[i-1][j  ].wart=='.' || mapa[i-1][j  ].wart =='X') && !mapa[i-1][j  ].G ){
+                 mapa[i-1][j  ].G = 1;
+                if (!eval_move(i-1, j  , start_path+1, moves_num+1)) mapa[i-1][j  ].G = 0;
+            }
+
+            if ((mapa[i+1][j  ].wart=='.' || mapa[i+1][j  ].wart =='X') && !mapa[i+1][j  ].D ){
+                 mapa[i+1][j  ].D = 1;
+                if (!eval_move(i+1, j  , start_path+1, moves_num+1)) mapa[i+1][j  ].D = 0;
+            }
+
+            if ((mapa[i  ][j-1].wart=='.' || mapa[i  ][j-1].wart =='X') && !mapa[i  ][j-1].L ){
+                 mapa[i  ][j-1].L = 1;
+                if (!eval_move(i  , j-1, start_path+1, moves_num+1)) mapa[i  ][j-1].L = 0;
+            }
+
+            if ((mapa[i  ][j+1].wart=='.' || mapa[i  ][j+1].wart =='X') && !mapa[i  ][j+1].P ){
+                 mapa[i  ][j+1].P = 1;
+                if (!eval_move(i  , j+1, start_path+1, moves_num+1)) mapa[i  ][j+1].P = 0;
+            }
+
             eval_move(i  , j  , start_path+1, moves_num);
+
             break;
     }
 
     return 0;
 }
 
-//int eval_path(int i, int j, int start_path){
-////    for (int k=start_path; k<L; k++){
-//
-//
-//    if (sekwencja[start_path]=='?'){
-//
-//        sekwencja[start_path]='?';
-//        eval_move(i, j, start_path);
-//
-//    }
-//    return 0;
-//}
+void zeruj_mape(void){
+
+    for (int i=0; i<M; i++){
+        for (int j=0; j<N; j++){
+            mapa[i][j].G = 0;
+            mapa[i][j].D = 0;
+            mapa[i][j].L = 0;
+            mapa[i][j].P = 0;
+
+            }
+        }
+}
 
 
 int find_endpoint(){
 
     for (int i=0; i<M; i++){
         for (int j=0; j<N; j++){
-            if (mapa[i][j]=='.' || mapa[i][j]=='X'){
-                directions[0] = directions[1] = directions[2] = directions[3] = 0;
+            if (mapa[i][j].wart=='.' || mapa[i][j].wart=='X'){
+
+                zeruj_mape();
+
                 eval_move( i, j, 0, 0);
             }
         }
